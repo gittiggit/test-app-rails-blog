4e24ca29c8f2dac1760752153bc40436475e3051
gittiggit <mailtommrr@gmail.com>
2016-04-10 21:40:16 +0600

    Commited after using helper method for title

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,14 @@
 module ApplicationHelper
+	def full_title(page_title = " ")
+		base_title = "Ruby on Rails Tutorial Sample App"
+		if page_title.empty?
+			base_title
+		else
+			page_title + " | "+ base_title
+		end
+	end
+
+	def demo_helper_app_helper_file
+		name = "Md. Mahmudur Rahman"
+	end
 end
