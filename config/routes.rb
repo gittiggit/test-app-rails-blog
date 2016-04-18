0aad61b802fd3327c53ed6308a8fad825a7cc478
gittiggit <mailtommrr@gmail.com>
2016-04-18 22:25:04 +0600

    Commited after adding a new user

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,11 @@
 Rails.application.routes.draw do
 
-  get 'users/new'
-
   root "staticpages#home"
   get 'users/new'
   get "home", to: 'staticpages#home'
-
   get "help", to: 'staticpages#help'
-
   get "about", to: 'staticpages#about'
-
 	get "signup", to: "users#new"
-
+  resources :users
 
 end
